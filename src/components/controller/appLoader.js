57b6fe5aa4d40867655958b144b3baa44b7a46df
Nikita <snitkonikita@gmail.com>
2023-07-11 19:55:50 +0400

    feat: the code has been prepared for operation

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
-        });
-    }
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: '93d5fc0490724111bea31715894332d3',
+    });
+  }
 }
 
 export default AppLoader;
