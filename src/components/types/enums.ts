7b24a7813f21ceddeb1475dfae1591bbe95faf91
Nikita <snitkonikita@gmail.com>
2023-07-12 20:14:14 +0400

    feat: create enums, interfaces and types files with data

---
+++ src/components/types/enums.ts
@@ -0,0 +1,12 @@
+export enum CheckID {
+  category = '#category',
+  country = '#country',
+  language = '#language',
+  name = '#name',
+}
+export enum CheckCategoryName {
+  category = 'category',
+  country = 'country',
+  language = 'language',
+  name = 'name',
+}
