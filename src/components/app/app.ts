c46320f435ce4cc94eaa37b122209f2e24e8ef49
Nikita <snitkonikita@gmail.com>
2023-07-18 14:05:39 +0400

    feat: add logic for filter

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -2,6 +2,7 @@ import { IApp } from 'components/types/interfaces';
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 import FilterLoader from 'components/filter/filterLoader';
+import { CheckID } from 'components/types/enums';
 
 class App implements IApp {
   private controller: AppController;
@@ -31,18 +32,49 @@ class App implements IApp {
       });
 
     const receiveMobileNews = (e: Event) => {
-      this.controller.
-    }
+      this.controller.getMobileNews({
+        e,
+        callback: (data) => {
+          if (data) {
+            this.view.drawNews(data);
+          }
+        },
+      });
+    };
+
+    // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+    const receiveNewsMobile_2 = (_e: Event) => {
+      dataListInput!.value = '';
+    };
+
     if (window.innerWidth >= 1000) {
       if (buttonList) {
         buttonList.addEventListener('click', receiveNews);
       }
     }
+    if (dataListInput) {
+      dataListInput.addEventListener('change', receiveMobileNews);
+      dataListInput.addEventListener('click', receiveNewsMobile_2);
+    }
 
     this.controller.getSources({
       callback: (data) => {
         if (data) {
+          this.filter.addCheckArea(data);
           this.view.drawSources(data);
+          const category: HTMLElement | null = document.querySelector(CheckID.category);
+          const country: HTMLElement | null = document.querySelector(CheckID.country);
+          const language: HTMLElement | null = document.querySelector(CheckID.language);
+          const name: HTMLElement | null = document.querySelector(CheckID.name);
+          [category, country, language, name].forEach((element) => {
+            element?.addEventListener('change', () => {
+              if (buttonList) buttonList.innerHTML = '';
+              if (dataListInput) dataListInput.value = '';
+              if (datalistList) datalistList.innerHTML = '';
+              if (newsList) newsList.innerHTML = '';
+              this.view.drawSources(data);
+            });
+          });
         }
       },
     });
