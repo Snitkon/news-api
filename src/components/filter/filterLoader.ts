7442db3ff6049b182101f4a33b2f196cda114f3c
Nikita <snitkonikita@gmail.com>
2023-07-18 12:20:14 +0400

    feat: add filter

---
+++ src/components/filter/filterLoader.ts
@@ -0,0 +1,55 @@
+import { CheckCategoryName } from 'components/types/enums';
+import { IData, IFilterloader, ISourceData } from 'components/types/interfaces';
+import { INewsSourceObject } from 'components/types/types';
+
+class FilterLoader implements IFilterloader {
+  addCheckArea(data: IData): void {
+    if (data.sources) {
+      const sourcesArray: Required<ISourceData[]> = data.sources;
+      const checkCategory: HTMLElement | null = document.querySelector('.check__type');
+      const newsSourceObject: INewsSourceObject<string> = {
+        name: new Set(),
+        category: new Set(),
+        country: new Set(),
+        language: new Set(),
+      };
+      for (const key in newsSourceObject) {
+        const newsCategoryName: HTMLElement = document.createElement('p3');
+        const newsCategory: HTMLElement = document.createElement('select');
+        if (checkCategory) {
+          checkCategory.append(newsCategoryName);
+          checkCategory.append(newsCategory);
+          newsCategory.classList.add('news__category_select');
+          newsCategory.id = key as string;
+          newsCategoryName.classList.add('news__category_name');
+          newsCategoryName.innerHTML = ('News ' + key) as string;
+        }
+      }
+      newsSourceObject.category.add('all');
+      newsSourceObject.country.add('all');
+      newsSourceObject.language.add('all');
+      newsSourceObject.name.add('all');
+      sourcesArray.forEach((element) => {
+        newsSourceObject.category.add(element[CheckCategoryName.category]);
+        newsSourceObject.country.add(element[CheckCategoryName.country]);
+        newsSourceObject.language.add(element[CheckCategoryName.language]);
+        newsSourceObject.name.add(element[CheckCategoryName.name]);
+      });
+      this.addCategory(CheckCategoryName.category, newsSourceObject[CheckCategoryName.category]);
+      this.addCategory(CheckCategoryName.country, newsSourceObject[CheckCategoryName.country]);
+      this.addCategory(CheckCategoryName.language, newsSourceObject[CheckCategoryName.language]);
+      this.addCategory(CheckCategoryName.name, newsSourceObject[CheckCategoryName.name]);
+    }
+  }
+  addCategory(category: string, info: object): void {
+    (info as string[]).forEach((element) => {
+      const catalogName: HTMLElement | null = document.querySelector(`#${category}`);
+      const elemOfSourceArray: HTMLElement = document.createElement('option');
+      elemOfSourceArray.classList.add('.option');
+      catalogName?.append(elemOfSourceArray);
+      elemOfSourceArray.innerHTML = element;
+    });
+  }
+}
+
+export default FilterLoader;
---
+++ src/components/filter/newsFilter.ts
@@ -0,0 +1,63 @@
+import { CheckID } from 'components/types/enums';
+import { INewsFilter, ISourceData } from 'components/types/interfaces';
+
+export class NewsFilter implements INewsFilter {
+  private category: HTMLSelectElement | null;
+  private country: HTMLSelectElement | null;
+  private language: HTMLSelectElement | null;
+  private name: HTMLSelectElement | null;
+  private filterArray: string[];
+  constructor() {
+    this.category = null;
+    this.country = null;
+    this.language = null;
+    this.name = null;
+    this.filterArray = [];
+  }
+  filter(): string[] {
+    this.category = document.querySelector(CheckID.category);
+    this.country = document.querySelector(CheckID.country);
+    this.language = document.querySelector(CheckID.language);
+    this.name = document.querySelector(CheckID.name);
+    if (this.name) {
+      this.filterCategory(this.name, 0);
+    }
+    if (this.category) {
+      this.filterCategory(this.category, 1);
+    }
+    if (this.country) {
+      this.filterCategory(this.country, 2);
+    }
+    if (this.language) {
+      this.filterCategory(this.language, 3);
+    }
+
+    return this.filterArray;
+  }
+  filterCategory(category: HTMLSelectElement, position: number): void {
+    this.filterArray[position] = category.value;
+    category?.addEventListener('change', () => {
+      if (category) {
+        this.filterArray[position] = category.value;
+      }
+    });
+  }
+
+  isInclude(arr: string[], item: ISourceData, i: number) {
+    return Object.values(item).includes(arr[i]) || arr[i] === 'all' ? true : false;
+  }
+
+  filterData(values: Required<ISourceData[]>): Required<ISourceData[]> {
+    const filterDataArray: string[] = this.filter();
+    const filterValues: Required<ISourceData[]> = [];
+
+    values.forEach((item) => {
+      const isIncludeName: boolean = this.isInclude(filterDataArray, item, 0);
+      const isIncludeCategory: boolean = this.isInclude(filterDataArray, item, 1);
+      const isIncludeCountry: boolean = this.isInclude(filterDataArray, item, 2);
+      const isIncludeLanguage: boolean = this.isInclude(filterDataArray, item, 3);
+      if (isIncludeName && isIncludeCategory && isIncludeCountry && isIncludeLanguage) filterValues.push(item);
+    });
+    return filterValues;
+  }
+}
